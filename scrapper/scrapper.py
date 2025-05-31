218c19ba8ce179fa54600151bd815480102fc552
theKyrylo <zhulin.pers0n4l@gmail.com>
2025-05-31 05:26:28 +0200

    DATAAAAAAA

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 # Function to fetch and parse the webpage
@@ -15,28 +16,57 @@ def fetch_and_parse(url):
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
         return None
 
-# Example function to scrape specific elements (change based on your needs)
-def scrape_data(soup):
-    # Find the title of the page
-    title = soup.find('title').get_text()
-    print(f"Title of the page: {title}")
+counter = 0
+json_data = []
+# AFISHA.PL
+def parse_event(url):
+    global json_data
+    global counter
+    soup = fetch_and_parse(url)
 
-    # Example: Scraping all links (anchor tags)
-    links = soup.find_all('a')
-    for link in links:
-        href = link.get('href')
-        if href:
-            print(f"Link: {href}")
+    description = soup.find(class_='description').find('strong')
+    if description is None:
+        description = ""
+    else:
+        description = description.text
 
-    # Example: Scraping all paragraphs
-    paragraphs = soup.find_all('p')
-    for para in paragraphs:
-        print(f"Paragraph: {para.get_text()}")
+    data = {
+        "title": soup.find(class_='page-header-title').text,
+        "data": soup.find(class_='calendar-container').get('data-start'),
+        "description": description,
+        "location": soup.find(class_='calendar-container').get('data-location'),
+        "category": soup.find_all(class_='list-inline-item text-nowrap')[-1].text,
+        "url": url
+    }
+
+    json_data.append(data)
+
+    #DEBUG
+    counter += 1
+
+    #print(counter, url, title, date, description, location, category)
+
+def find_event_links(soup):
+    events_div = soup.find(class_='events')
+    link_divs = events_div.find_all('div', {'class': 'eventDescription'})
+    for div in link_divs:
+        link = div.find('a').get('href')
+        parse_event("https://www.afisha.pl" + link)
+
+def parse_pages(soup):
+    find_event_links(soup)
+    sibling = soup.find(class_='page-numbers current').find_next()
+    if sibling.name == "a":
+        print('NEWPAGE')
+        parse_pages(fetch_and_parse(sibling.get('href')))
 
 # Main function to drive the script
 if __name__ == '__main__':
-    url = 'https://www.krakow.pl/kalendarium/1919,artykul,kalendarium.html'
+    url = 'https://www.afisha.pl/location/Krak%C3%B3w/'
     soup = fetch_and_parse(url)
 
     if soup:
-        scrape_data(soup)
+        parse_pages(soup)
+        with open('data.json', 'w') as f:
+            json.dump(json_data, f, indent=4)
+        # scrape_data(soup)
